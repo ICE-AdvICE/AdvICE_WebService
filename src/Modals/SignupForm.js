4c838fd1ad4baa689d5b61492530cb10e3ac9c2e
Debuging-JunSeoPark <81576126+Debuging-JunSeoPark@users.noreply.github.com>
2024-03-28 23:22:18 +0900

    Add files via upload

---
+++ src/Modals/SignupForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styles from './login.modules.css';
+
+
+const SignUpForm = ({ onSignUpForm }) => {
+  const [user_email, setUseremail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    SignUpForm(user_email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="signUpContainer">
+      <img
+                    className="signinIcon"
+                    src="./HufsLogo.png"
+                  />       
+      <p>이메일</p>
+        <label htmlFor="user_email" ></label>
+        
+        <input
+          type="text"
+          id="user_email"
+          value={user_email}
+          onChange={e => setUseremail(e.target.value)}
+          placeholder="학교 이메일을 입력해주세요."
+
+        />
+        <button type="submit"  className="post">인증요청</button>
+      </div>
+
+      <div className="emailpost" >
+      <p>인증번호</p>
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="인증번호를 입력해주세요."
+        />
+        <button type="submit"  className="post">인증</button>
+      </div>
+    
+      
+
+        <button type="submit"  className="signupButton">다음단계</button>
+      
+     
+      
+    </form>
+  );
+};
+
+
+export default SignUpForm;
