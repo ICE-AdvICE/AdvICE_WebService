2f3f6f71d3106ec92f500eefeedd32d34cf31f7c
이윤빈 <kanado20000@naver.com>
2024-04-03 04:45:55 +0900

    회원가입 & 로그인 API 구현

--- backend/src/main/java/com/icehufs/icebreaker/entity/Article.java
+++ backend/src/main/java/com/icehufs/icebreaker/entity/Article.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,6 +21,7 @@ import lombok.ToString;
 @ToString
 @Getter
 @Entity
+@Table(name = "article")
 public class Article {
 
     
