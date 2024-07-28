6589cbaee44bc55098c0d059ad9cd9d7d1166734
이윤빈 <kanado20000@naver.com>
2024-07-28 16:23:38 +0900

    운영자 권한 종류 확인 GET API 구현 완료

--- backend/src/main/java/com/icehufs/icebreaker/dto/response/codingzone/AuthorityExistResponseDto.java
+++ backend/src/main/java/com/icehufs/icebreaker/dto/response/codingzone/AuthorityExistResponseDto.java
@@ -22,9 +22,14 @@ public class AuthorityExistResponseDto extends ResponseDto{
         ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
     }
-    public static ResponseEntity<ResponseDto> notAdmin(){
-        ResponseDto result = new ResponseDto(ResponseCode.SUCCESS_BUT_NOT, ResponseMessage.SUCCESS_BUT_NOT);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+    public static ResponseEntity<ResponseDto> codingAdmin(){
+        ResponseDto result = new ResponseDto(ResponseCode.CODING_ADMIN, ResponseMessage.CODING_ADMIN);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> entireAdmin(){
+        ResponseDto result = new ResponseDto(ResponseCode.ENTIRE_ADMIN, ResponseMessage.ENTIRE_ADMIN);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
     
 }
