eee286b8fa31fb77f669aacb8c8b531eae919ba0
이윤빈 <kanado20000@naver.com>
2024-08-19 19:39:59 +0900

    권한 부여/박탈 페이지 구현 및 API 연동, 메인 페이지 피드백 버튼 추가

--- frontend/src/apis/Codingzone-api.js
+++ frontend/src/apis/Codingzone-api.js
@@ -343,3 +343,43 @@ export const getczallattendRequest = async (accessToken) => {
         return error.response.data;
     }
 };
+
+//권한 부여 API
+export const grantPermission = async (email, role, token) => {
+    try {
+        const response = await axios.patch(`${API_DOMAIN_ADMIN}/give-auth`, {
+            email,
+            role
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        if (!error.response) {
+            return { code: 'NETWORK_ERROR', message: '네트워크 상태를 확인해주세요.' };
+        }
+        return error.response.data;
+    }
+}
+
+//권한 박탈 API
+export const deprivePermission = async (email, role, token) => {
+    try {
+        const response = await axios.patch(`${API_DOMAIN_ADMIN}/deprive-auth`, {
+            email,
+            role
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        if (!error.response) {
+            return { code: 'NETWORK_ERROR', message: '네트워크 상태를 확인해주세요.' };
+        }
+        return error.response.data;
+    }
+}
