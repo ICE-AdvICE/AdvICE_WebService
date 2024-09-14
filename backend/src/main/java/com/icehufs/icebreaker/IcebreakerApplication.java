d4223f48e5f956965b5140c3aa29585feecf6f01
이윤빈 <kanado20000@naver.com>
2024-09-14 12:21:27 +0900

    데이터 암호화 .env

--- backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
+++ backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
@@ -1,18 +1,25 @@
 package com.icehufs.icebreaker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-
 @EnableScheduling
 @SpringBootApplication
 public class IcebreakerApplication {	
-	public static void main(String[] args) {
-		SpringApplication.run(IcebreakerApplication.class, args);
-	}	
+    public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure()
+                              .directory("backend/") // .env 파일의 경로를 지정
+                              .load();
+
+        // 환경 변수를 시스템 프로퍼티로 설정
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(IcebreakerApplication.class, args);
+    }	
 }
-
-
 
