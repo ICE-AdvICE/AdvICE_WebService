225ba98a92c35421b7bd34404ce5edce96585537
김진우 <jinwookim85@naver.com>
2025-02-08 22:11:37 +0900

    Feat:게시글 6개  이하일 경우 페이지네이션 숨김

--- frontend/src/widgets/layout/Pagination/Pagination.js
+++ frontend/src/widgets/layout/Pagination/Pagination.js
@@ -1,28 +1,37 @@
 const Pagination = ({ paginate, currentPage, totalPages }) => {
     const handleClick = (e, number) => {
         e.preventDefault();
-        if (number < 1 || number > totalPages) return; //  
+        if (number < 1 || number > totalPages) return;
         paginate(number);
     };
 
-    
+    if (totalPages <= 1) {
+        return null;  
+    }
+
     return (
-        <nav aria-label="Page navigation example"className="pagination-bar">
+        <nav aria-label="Page navigation example" className="pagination-bar">
             <ul className="pagination">
-                <li className={currentPage === 1 ? "page-item disabled" : "page-item"}>
-                    <a className="page-link" onClick={(e) => handleClick(e, currentPage - 1)}>이전</a>
-                </li>
+                {totalPages > 1 && (
+                    <li className={currentPage === 1 ? "page-item disabled" : "page-item"}>
+                        <a className="page-link" onClick={(e) => handleClick(e, currentPage - 1)}>이전</a>
+                    </li>
+                )}
+
                 {Array.from({ length: totalPages }, (_, i) => (
                     <li key={i + 1} className={currentPage === i + 1 ? "page-item active" : "page-item"}>
                         <a className="page-link" onClick={(e) => handleClick(e, i + 1)}>{i + 1}</a>
                     </li>
                 ))}
-                <li className={currentPage === totalPages ? "page-item disabled" : "page-item"}>
-                    <a className="page-link"  onClick={(e) => handleClick(e, currentPage + 1)}>다음</a>
-                </li>
+
+                {totalPages > 1 && (
+                    <li className={currentPage === totalPages ? "page-item disabled" : "page-item"}>
+                        <a className="page-link" onClick={(e) => handleClick(e, currentPage + 1)}>다음</a>
+                    </li>
+                )}
             </ul>
         </nav>
     );
-}
+};
 
 export default Pagination;
