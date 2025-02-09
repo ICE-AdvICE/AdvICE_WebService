26931bf1902e0768c54ab3fdbb3d67b7b32fd714
이윤빈 <kanado20000@naver.com>
2025-02-10 01:26:33 +0900

    feature: DTOResponse 클래스 구현

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/dto/response/DownloadArticleExcelResponseDto.java
@@ -0,0 +1,27 @@
+package com.icehufs.icebreaker.domain.codingzone.dto.response;
+
+import com.icehufs.icebreaker.common.ResponseDto;
+import com.icehufs.icebreaker.common.ResponseCode;
+import com.icehufs.icebreaker.common.ResponseMessage;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class DownloadArticleExcelResponseDto extends ResponseDto {
+
+    private DownloadArticleExcelResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<DownloadArticleExcelResponseDto> success() {
+        DownloadArticleExcelResponseDto result = new DownloadArticleExcelResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> failed() {
+        ResponseDto result = new ResponseDto(ResponseCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+    }
+}
