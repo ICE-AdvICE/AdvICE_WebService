96d097ffedaac6547ada3b74c27d578fa4e9e43c
이윤빈 <kanado20000@naver.com>
2025-02-10 03:34:31 +0900

    refactor: 권한 있는 controller 패키지 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/auth/controller/CodingZoneClass2Controller.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/auth/controller/CodingZoneClass2Controller.java
@@ -1,4 +1,4 @@
-package com.icehufs.icebreaker.domain.codingzone.controller;
+package com.icehufs.icebreaker.domain.auth.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
