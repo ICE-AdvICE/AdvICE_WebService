ee885efab6f5816d10db1d18334ab4b67ebc73f4
이윤빈 <kanado20000@naver.com>
2025-02-22 03:00:53 +0900

    hotfix: 버그 발생 코드 비활성화

--- backend/src/main/java/com/icehufs/icebreaker/config/CorsConfig.java
+++ backend/src/main/java/com/icehufs/icebreaker/config/CorsConfig.java
@@ -6,21 +6,21 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.addAllowedOrigin("http://localhost:3000");
-		corsConfiguration.addAllowedMethod("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addExposedHeader("Set-Cookie");
+//     @Bean
+//     public CorsConfigurationSource corsConfigurationSource() {
+//         CorsConfiguration corsConfiguration = new CorsConfiguration();
+//         corsConfiguration.setAllowCredentials(true);
+//         corsConfiguration.addAllowedOrigin("http://localhost:3000");
+//         corsConfiguration.addAllowedMethod("*");
+//         corsConfiguration.addAllowedHeader("*");
+//         corsConfiguration.addExposedHeader("Set-Cookie");
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", corsConfiguration);
 
-		return source;
-	}
-}
+//         return source;
+//     }
+// }
