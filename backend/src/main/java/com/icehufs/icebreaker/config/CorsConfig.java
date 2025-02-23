fd0c654e42d4d5e95527c21cbc2a08cca796cb07
glaxyt <glaxyt@naver.com>
2025-02-23 21:23:01 +0900

    hotfix: CorsConfig를 WebMvcConfigurer로 관리

--- backend/src/main/java/com/icehufs/icebreaker/config/CorsConfig.java
+++ backend/src/main/java/com/icehufs/icebreaker/config/CorsConfig.java
@@ -1,26 +1,24 @@
 package com.icehufs.icebreaker.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
-
-//     @Bean
-//     public CorsConfigurationSource corsConfigurationSource() {
-//         CorsConfiguration corsConfiguration = new CorsConfiguration();
-//         corsConfiguration.setAllowCredentials(true);
-//         corsConfiguration.addAllowedOrigin("http://localhost:3000");
-//         corsConfiguration.addAllowedMethod("*");
-//         corsConfiguration.addAllowedHeader("*");
-//         corsConfiguration.addExposedHeader("Set-Cookie");
-
-//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//         source.registerCorsConfiguration("/**", corsConfiguration);
-
-//         return source;
-//     }
-// }
+@Configuration
+public class CorsConfig {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOriginPatterns("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true);
+			}
+		};
+	}
+}
