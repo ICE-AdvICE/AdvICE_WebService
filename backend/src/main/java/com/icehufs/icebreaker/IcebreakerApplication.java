2ab4b9caed2ad68638f1efa01eee7843f867a173
glaxyt <glaxyt@naver.com>
2025-03-01 19:48:39 +0900

    hotfix: 마운트 로직 수정 및 main에서 이미 마운트된 환경변수를 다시 load하지 않도록 수정

--- backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
+++ backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
@@ -10,13 +10,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class IcebreakerApplication {	
     public static void main( String[] args) {
         // .env 파일 로드
-        Dotenv dotenv = Dotenv.configure().directory("/app/").load();
-
+        Dotenv dotenv = Dotenv.configure()
+            .directory("/app") // 컨테이너 내부 작업 디렉토리
+            .ignoreIfMissing() // 파일이 없으면 무시
+            .load();
 
         // 환경 변수를 시스템 프로퍼티로 설정
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        if (dotenv != null) {
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        }
 
         SpringApplication.run(IcebreakerApplication.class, args);
     }	
