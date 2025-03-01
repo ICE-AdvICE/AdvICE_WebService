fedbf89b9578ef697f9d5bf9500a7087c1de7fac
glaxyt <glaxyt@naver.com>
2025-03-01 18:43:44 +0900

    hotfix: 환경변수 경로 app으로 수정

--- backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
+++ backend/src/main/java/com/icehufs/icebreaker/IcebreakerApplication.java
@@ -10,9 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class IcebreakerApplication {	
     public static void main( String[] args) {
         // .env 파일 로드
-        Dotenv dotenv = Dotenv.configure()
-                              .directory("backend/") // .env 파일의 경로를 지정
-                              .load();
+        Dotenv dotenv = Dotenv.configure().directory("/app/").load();
+
 
         // 환경 변수를 시스템 프로퍼티로 설정
         dotenv.entries().forEach(entry -> {
