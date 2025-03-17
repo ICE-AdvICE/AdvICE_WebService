2a0e95ecb75d4e5af0f5e9163d3b91744f4a10e0
glaxyt <glaxyt@naver.com>
2025-03-18 01:48:28 +0900

    fix: 레디스 비밀번호 설정

--- backend/src/main/java/com/icehufs/icebreaker/config/RedisConfig.java
+++ backend/src/main/java/com/icehufs/icebreaker/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,8 +18,13 @@ public class RedisConfig {
 	@Value("${spring.redis.port}")
 	private int port;
 
+	@Value("${spring.redis.password}")
+	private String password;
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+		redisConfig.setPassword(password);
+
 		return new LettuceConnectionFactory(host, port);
 	}
 
